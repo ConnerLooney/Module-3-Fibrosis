23c8078957e57074867671062855ee4784766d63
ConnerLooney <tca6ng@virginia.edu>
2026-03-23 11:25:02 -0400

    updated main
    This is an updated main file that prints a csv and was made more efficient by chat

--- code/main_example.py
+++ code/main_example.py
@@ -1,15 +1,9 @@
-#AI Usage Statement: AI was used to help Debug code
-
-'''Module 3: count black and white pixels and compute the percentage of white pixels in a .jpg image and extrapolate points'''
+#AI Usage Statement: AI was used to help Debug code and increase efficiency
 
 from termcolor import colored
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 import pandas as pd
-
-# Load the images you want to analyze
 
 filenames = [
     r"/Users/connerlooney/Documents/GitHub/Module-3-Fibrosis/images/MASK_Sk658 Llobe ch010017.jpg",
@@ -20,80 +14,48 @@ filenames = [
     r"/Users/connerlooney/Documents/GitHub/Module-3-Fibrosis/images/MASK_Sk658 Llobe ch010023.jpg",
 ]
 
-# Enter the depth of each image (in the same order that the images are listed above; you can find these in the .csv file provided to you which is tilted: "Filenames and Depths for Students")
+depths = [45, 90, 60, 30, 80, 100]
 
-depths = [
-    45,
-    90,
-    60,
-    30,
-    80,
-    100
-]
-
-# Make the lists that will be used
-
-images = []
-white_counts = []
-black_counts = []
 white_percents = []
 
-# Build the list of all the images you are analyzing
+print(colored("Counts of pixel color in each image", "yellow"))
 
-for filename in filenames:
+for filename, depth in zip(filenames, depths):
     img = cv2.imread(filename, 0)
-    images.append(img)
 
-# For each image (until the end of the list of images), calculate the number of black and white pixels and make a list that contains this information for each filename.
+    if img is None:
+        print(f"Error loading image: {filename}")
+        continue
 
-for x in range(len(filenames)):
-    _, binary = cv2.threshold(images[x], 127, 255, cv2.THRESH_BINARY)
+    # Threshold
+    _, binary = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
-    white = np.sum(binary == 255)
-    black = np.sum(binary == 0)
+    # Count pixels efficiently
+    white = np.count_nonzero(binary == 255)
+    total = binary.size
+    black = total - white
 
-    white_counts.append(white)
-    black_counts.append(black)
-
-# Print the number of white and black pixels in each image.
-
-print(colored("Counts of pixel by color in each image", "yellow"))
-for x in range(len(filenames)):
-    print(colored(f"White pixels in image {x}: {white_counts[x]}", "white"))
-    print(colored(f"Black pixels in image {x}: {black_counts[x]}", "black"))
-    print()
-
-# Calculate the percentage of pixels in each image that are white and make a list that contains these percentages for each filename
-
-for x in range(len(filenames)):
-    white_percent = (
-        100 * (white_counts[x] / (black_counts[x] + white_counts[x])))
+    # Compute percent
+    white_percent = 100 * white / total
     white_percents.append(white_percent)
 
-# Print the filename (on one line in red font), and below that line print the percent white pixels and depth into the lung where the image was obtained
+    # Print (same style as before)
+    print(colored(f"\n{filename}:", "red"))
+    print(colored(f"White pixels: {white}", "white"))
+    print(colored(f"Black pixels: {black}", "grey"))
+    print(f"{white_percent:.2f}% White | Depth: {depth} microns")
 
-print(colored("Percent white px:", "yellow"))
-for x in range(len(filenames)):
-    print(colored(f'{filenames[x]}:', "red"))
-    print(f'{white_percents[x]}% White | Depth: {depths[x]} microns')
-    print()
-
-'''Write your data to a .csv file'''
-
-# Create a DataFrame that includes the filenames, depths, and percentage of white pixels
+# Create DataFrame (UNCHANGED STRUCTURE)
 df = pd.DataFrame({
     'Filenames': filenames,
     'Depths': depths,
     'White percents': white_percents
 })
 
-# Write that DataFrame to a .csv file
-
+# Save CSV (same as before)
 df.to_csv('Percent_White_Pixels.csv', index=False)
 
-print("The .csv file 'Percent_White_Pixels.csv' has been created.")
-
-'''the .csv writing subroutine ends here'''
+print("\nThe .csv file 'Percent_White_Pixels.csv' has been created.")
 
 
 ##############
